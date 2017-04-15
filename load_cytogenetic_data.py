041bda88a8a8704115fb51deb2100a6ca6a4dfa9
Kevin Shi <xpspectre@gmail.com>
2017-04-15 16:00:58 -0400

    Finished cytogenetic data processing (more or less) 2 outputs: a detailed csv with all the features and a summary csv with just any abnormality: yes/no

--- load_cytogenetic_data.py
+++ load_cytogenetic_data.py
@@ -6,10 +6,10 @@ import pandas as pd
 import numpy as np
 
 
-def pivot_dataset(df, col, exclude=[], missing=None):
+def pivot_dataset(df, col, exclude=[], missing=[]):
     """Pivot dataframe df of Patient with column col of categoricals into n_categories columns of 1/0. Specify
-    list of cols to exclude (such as redundant ones). missing is the value that indicates no data for that
-    patient."""
+    list of cols to exclude (such as redundant ones). Specify list of missing col names that indicate the patient
+    data is missing or noninformative."""
     x = df.pivot(index='Patient', columns=col, values=col)
 
     # Drop redundant columns
@@ -21,9 +21,9 @@ def pivot_dataset(df, col, exclude=[], missing=None):
     x = x.apply(pd.to_numeric, errors='ignore', downcast='float')
 
     # Set patients with Missing = 1 to all NaNs and drop the Missing row
-    if missing is not None:
-        x.loc[x[missing] == 1, :] = np.nan
-        x.drop(missing, axis=1, inplace=True)
+    for mis in missing:
+        x.loc[x[mis] == 1, :] = np.nan
+        x.drop(mis, axis=1, inplace=True)
 
     return x
 
@@ -52,6 +52,7 @@ def merge_datasets_merge_cols(df1, df2):
 if __name__ == '__main__':
     # Files
     data_dir = 'data/raw/cytogenetic_data'
+    results_dir = 'data/processed'
 
     abn_fish_base = 'mmrf_explore_ia9_cytogenetics_abnormalities_fish_'
     abn_fish_times = [
@@ -67,14 +68,19 @@ if __name__ == '__main__':
         'baseline'
     ]
 
-    trisomies_base = 'mmrf_explore_ia9_cytogenetics_trisomies_'
-    trisomies_times = [
+    tris_base = 'mmrf_explore_ia9_cytogenetics_trisomies_'
+    tris_times = [
+        'baseline'
+    ]
+
+    aneu_base = 'mmrf_explore_ia9_cytogenetics_aneuploidy_'
+    aneu_times = [
         'baseline'
     ]
 
     # Load baseline FISH data
     abn_fish_raw = pd.read_csv(os.path.join(data_dir, abn_fish_base + abn_fish_times[0] + '.csv'))
-    abn_fish = pivot_dataset(abn_fish_raw, 'Abnormalities (FISH) - Baseline', exclude=['Multiple', 'None'], missing='Missing')
+    abn_fish = pivot_dataset(abn_fish_raw, 'Abnormalities (FISH) - Baseline', exclude=['Multiple', 'None'], missing=['Missing'])
 
     # Load baseline META data
     abn_meta_raw = pd.read_csv(os.path.join(data_dir, abn_meta_base + abn_meta_times[0] + '.csv'))
@@ -85,23 +91,41 @@ if __name__ == '__main__':
     # Combine FISH and META. An abnormality is present if it's a 1 in either of the datasets
     abn_comb = merge_datasets_merge_cols(abn_fish, abn_meta)
 
-    # TODO: Grab the Other abnormalities in the META dataset
+    # Grab the Other abnormalities in the META dataset
+    abn_meta_other = abn_meta_raw.loc[pd.notnull(abn_meta_raw['Other']), ['Patient', 'Other']]
+    abn_meta_other.set_index('Patient', inplace=True)
 
     # Load trisomies
-    trisomies_raw = pd.read_csv(os.path.join(data_dir, trisomies_base + trisomies_times[0] + '.csv'))
-    trisomies_raw.drop_duplicates(subset=['Patient', 'Trisomies - Baseline'], take_last=True, inplace=True)  # Delete duplicate info like in row 114 (of the csv)
-    trisomies = pivot_dataset(trisomies_raw, 'Trisomies - Baseline', exclude=[], missing='Not Done')
+    tris_raw = pd.read_csv(os.path.join(data_dir, tris_base + tris_times[0] + '.csv'))
+    tris_raw.drop_duplicates(subset=['Patient', 'Trisomies - Baseline'], take_last=True, inplace=True)  # Delete duplicate info like in row 114 (of the csv)
+    tris = pivot_dataset(tris_raw, 'Trisomies - Baseline', exclude=[], missing=['Not Done'])
 
     # Rename trisomies columns
-    trisomies.rename(columns=dict([(i, 'Trisomy_' + i) for i in list(trisomies)]), inplace=True)
+    tris.rename(columns=dict([(i, 'Trisomy_' + i) for i in list(tris)]), inplace=True)
 
-    # Combine FISH+META and trisomies.
-    abn_comb = abn_comb.join(trisomies, how='outer')
+    # Combine FISH+META and trisomies
+    abn_comb = abn_comb.join(tris, how='outer')
+
+    # Load aneuploidies
+    aneu_raw = pd.read_csv(os.path.join(data_dir, aneu_base + aneu_times[0] + '.csv'))
+    aneu = pivot_dataset(aneu_raw, 'Aneuploidy - Baseline', exclude=['None'], missing=['Not Done/Unknown'])
+
+    # Combine
+    abn_comb = abn_comb.join(aneu, how='outer')
+
+    # Add back in the meta Other text col
+    abn_comb = abn_comb.join(abn_meta_other, how='outer')
 
     # Save results
-    results_file = os.path.join(data_dir, 'data_test' + '.csv')
-    abn_comb.to_csv(results_file)
-
     print(abn_comb)
+    abn_comb.to_csv(os.path.join(results_dir, 'cytogenetic_data_details_processed' + '.csv'))
 
-    1
+    # Process summary file separately - coarser features
+    summ_base = 'mmrf_explore_ia9_cytogenetics_cytogenetic_summary_'
+    summ_times = [
+        'baseline'
+    ]
+    summ_raw = pd.read_csv(os.path.join(data_dir, summ_base + summ_times[0] + '.csv'))
+    summ = pivot_dataset(summ_raw, 'Cytogenetic Summary - Baseline', exclude=['Normal'], missing=['Not Done/Unknown', 'Indeterminant'])
+    print(summ)
+    summ.to_csv(os.path.join(results_dir, 'cytogenetic_data_summary_processed' + '.csv'))
