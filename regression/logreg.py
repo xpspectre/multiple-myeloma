c4319b17ed44aeef4b9efa5e127a5f147bf57226
Dustin Doss <ddoss@mit.edu>
2017-05-05 16:49:46 -0400

    Fixed dumb logreg outcome thing

--- regression/logreg.py
+++ regression/logreg.py
@@ -3,8 +3,8 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 
 study_data = pd.read_csv('./data/processed/patient_study.csv', index_col=0)
-#clinical_data = pd.read_csv('./data/processed/imputed_8.csv', index_col=0)
-clinical_data = pd.read_csv('./data/processed/baseline_clinical_data_imputed_mice.csv', index_col=0)
+clinical_data = pd.read_csv('./data/processed/imputed_8.csv', index_col=0)
+#clinical_data = pd.read_csv('./data/processed/baseline_clinical_data_imputed_knn.csv', index_col=0)
 treatment_data = pd.read_csv('./data/processed/patient_treat.csv', index_col=0)
 patient_endp = pd.read_csv('./data/processed/patient_endp.csv', index_col=0)
 
@@ -14,13 +14,15 @@ treatment_data = treatment_data.drop('sct_bresp', 1)
 # Only look at patients in all datasets, who were part of the study
 all_ids = list(set(patient_endp.index).intersection(set(treatment_data.index)).intersection(set(clinical_data.index)).intersection(set((study_data.STUDY_ID == 1).index)))
 
-y = pd.notnull(patient_endp[['D_PT_lstalive', 'D_PT_deathdy']].max(axis=1).loc[all_ids])
+y = patient_endp[['D_PT_lstalive', 'D_PT_deathdy']].max(axis=1).loc[all_ids]
+y = y.loc[pd.notnull(y)]
+y = y>365.
 all_ids = list(set(all_ids).intersection(y.index))
 X = clinical_data.join(treatment_data).loc[all_ids]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
 
-logreg = LogisticRegression(random_state=1, max_iter=1000, n_jobs=3)
+logreg = LogisticRegression(penalty='l2', random_state=1, max_iter=10000, n_jobs=3)
 logreg.fit(X_train, y_train)
 score_train = logreg.score(X_train, y_train)
 score_test = logreg.score(X_test, y_test)
