2c02676720bb1774b7c570b69db72f5435573298
Kevin Shi <xpspectre@gmail.com>
2017-05-15 23:42:03 -0400

    Run GSEA on cluster script update Point to the right number of job array entries for updated patients

--- run_gsea_cluster.sh
+++ run_gsea_cluster.sh
@@ -6,7 +6,7 @@
 #PBS -l mem=8192Mb,vmem=8192Mb
 #PBS -l walltime=0:1:00:00
 #PBS -N multiple_myeloma_gsea
-#PBS -t 1-676
+#PBS -t 1-664
 
 cd /data/kshi/multiple_myeloma/processed/gsea2
 ./run_${PBS_ARRAYID}.sh
